90070657f965193bdc6e7ecc54fb9fc5dc6199cf
Pranav <pranavraj1720@gmail.com>
2021-09-15 19:35:38 +0530

    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector("#menu-btn")
+let navbar = document.querySelector(".navbar")
+
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times")
+    navbar.classList.toggle("active")
+}
