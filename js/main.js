2b58daafa490140bc8c79f5254f8816a0c7e20e5
Pranav <pranavraj1720@gmail.com>
2021-09-18 10:20:41 +0530

    Features Section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ menu.onclick = () =>{
     menu.classList.toggle("fa-times")
     navbar.classList.toggle("active")
 }
+
+
+window.onscroll = () =>{
+    menu.classList.remove("fa-times")
+    navbar.classList.remove("active")
+}
